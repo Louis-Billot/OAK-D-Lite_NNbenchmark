94fc6705837a719017983d4672308c83f2940a77
Louis-Billot <96132026+Louis-Billot@users.noreply.github.com>
2022-05-02 15:49:19 +0200

    from test.cpp to main.cpp

--- host_decoding/main.cpp
+++ host_decoding/main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <iostream>
+#include <fstream>
 
 // Includes common necessary includes for development using depthai library
 #include "depthai/depthai.hpp"
@@ -7,7 +8,15 @@
 // #include "Windows.h"
 #include <windows.h>
 
-static const std::vector<std::string> labelMap = {
+#include <filesystem>
+
+using std::cout; using std::cin;
+using std::endl; using std::vector;
+using std::string;
+using std::filesystem::directory_iterator;
+using std::filesystem::directory_entry;
+
+static const vector<std::string> labelMap = {
     "person",        "bicycle",      "car",           "motorbike",     "aeroplane",   "bus",         "train",       "truck",        "boat",
     "traffic light", "fire hydrant", "stop sign",     "parking meter", "bench",       "bird",        "cat",         "dog",          "horse",
     "sheep",         "cow",          "elephant",      "bear",          "zebra",       "giraffe",     "backpack",    "umbrella",     "handbag",
@@ -23,8 +32,8 @@ static std::atomic<bool> syncNN{true};
 int main(int argc, char** argv) {
     using namespace std;
     using namespace std::chrono;
-    // std::string nnPath(BLOB_PATH);
-    std::string nnPath;
+    // string nnPath(BLOB_PATH);
+    string nnPath;
 
     // If path to blob specified, use that
     if(argc > 1) {
@@ -32,170 +41,188 @@ int main(int argc, char** argv) {
     }
     else return 1;
 
-    // Print which blob we are using
-    printf("Using blob at path: %s\n", nnPath.c_str());
+    string path = "c:/depthai_blob_files/test_dir/";
 
-    // Create pipeline
-    dai::Pipeline pipeline;
+    std::fstream fs;
+    fs.open (path + "test.txt", std::fstream::out | std::fstream::trunc);
 
-    // Define source and output
-    auto camRgb = pipeline.create<dai::node::ColorCamera>();
-    auto detectionNetwork = pipeline.create<dai::node::YoloDetectionNetwork>();
-    auto xoutRgb = pipeline.create<dai::node::XLinkOut>();
-    auto nnOut = pipeline.create<dai::node::XLinkOut>();
+    vector<directory_entry> files_list;
 
-    xoutRgb->setStreamName("rgb");
-    nnOut->setStreamName("detections");
-
-    // Properties
-    camRgb->setPreviewSize(640, 640);
-    camRgb->setBoardSocket(dai::CameraBoardSocket::RGB);
-    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
-    camRgb->setInterleaved(false);
-    camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
-    camRgb->setFps(30);
-
-    // Network specific settings
-    detectionNetwork->setConfidenceThreshold(0.5f);
-    detectionNetwork->setNumClasses(80);
-    detectionNetwork->setCoordinateSize(4);
-    detectionNetwork->setAnchors({10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326});
-    // detectionNetwork->setAnchorMasks({{"side80", {0, 1, 2}}, {"side40", {3, 4, 5}}, {"side20", {6, 7, 8}}});
-    detectionNetwork->setAnchorMasks({{"side85", {0, 1, 2}}, {"side85", {3, 4, 5}}, {"side85", {6, 7, 8}}});
-    detectionNetwork->setIouThreshold(0.5f);
-    detectionNetwork->setBlobPath(nnPath);
-    // detectionNetwork->setNumInferenceThreads(2);
-    detectionNetwork->input.setBlocking(false);
-
-    // Linking
-    camRgb->preview.link(detectionNetwork->input);
-    if(syncNN) {
-        detectionNetwork->passthrough.link(xoutRgb->input);
-    } else {
-        camRgb->preview.link(xoutRgb->input);
+    for (const auto & file : directory_iterator(path))
+    {
+        if (file.is_regular_file() && file.path().extension() == ".blob")
+            files_list.push_back(file);
+        cout << file.path().extension() << endl;
     }
 
-    detectionNetwork->out.link(nnOut->input);
+    for (const auto filepath : files_list)
+    {
+        cout << filepath.is_regular_file() << filepath.path() << endl;
+        nnPath = filepath.path().string();
+        
 
-    // Connect to device and start pipeline
-    dai::Device device(pipeline, dai::UsbSpeed::SUPER);
+        // Print which blob we are using
+        printf("Using blob at path: %s\n", nnPath.c_str());
+        fs << nnPath << endl;
 
-    cout << "Connected cameras: ";
-    for(const auto& cam : device.getConnectedCameras()) {
-        cout << cam << " ";
-    }
-    cout << endl;
+        const uint16_t nn_shape = 640;
 
-    // Print USB speed
-    cout << "Usb speed: " << device.getUsbSpeed() << endl;
+        const auto output_shape = int(3 * (pow((nn_shape/8.0), 2) + pow((nn_shape/16.0), 2) + pow((nn_shape/32.0), 2)));
 
-    // Output queue will be used to get the rgb frames from the output defined above
-    auto qRgb = device.getOutputQueue("rgb", 4, false);
-    auto qDet = device.getOutputQueue("detections", 4, false);
+        // Create pipeline
+        dai::Pipeline pipeline;
 
-    // LARGE_INTEGER time1, time2, freq, starttime, stoptime;
-    // QueryPerformanceFrequency(&freq);
+        // Define source and output
+        auto camRgb = pipeline.create<dai::node::ColorCamera>();
+        auto detectionNetwork = pipeline.create<dai::node::NeuralNetwork>();
+        auto xoutRgb = pipeline.create<dai::node::XLinkOut>();
+        auto nnOut = pipeline.create<dai::node::XLinkOut>();
 
-    // uint64_t frm_cnt = 0;
-    // QueryPerformanceCounter(&starttime);
+        detectionNetwork->setBlobPath(nnPath);
+        detectionNetwork->setNumPoolFrames(4);
+        detectionNetwork->input.setBlocking(false);
+        detectionNetwork->setNumInferenceThreads(2);
 
-    cv::Mat frame;
-    // std::vector<dai::ImgDetection> detections;
-    std::vector<float> detections;
-    auto startTime = steady_clock::now();
-    int counter = 0;
-    float fps = 0;
-    auto color2 = cv::Scalar(255, 255, 255);
+        xoutRgb->setStreamName("rgb");
+        xoutRgb->input.setBlocking(false);
 
-    // Add bounding boxes and text to the frame and show it to the user
-    auto displayFrame = [](std::string name, cv::Mat frame, std::vector<dai::ImgDetection>& detections) {
-    // auto displayFrame = [](std::string name, cv::Mat frame, std::vector<float>& detections) {
-        auto color = cv::Scalar(255, 0, 0);
-        // nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
-        for(auto& detection : detections) {
-            int x1 = detection.xmin * frame.cols;
-            int y1 = detection.ymin * frame.rows;
-            int x2 = detection.xmax * frame.cols;
-            int y2 = detection.ymax * frame.rows;
+        nnOut->setStreamName("nn");
 
-            uint32_t labelIndex = detection.label;
-            std::string labelStr = to_string(labelIndex);
-            if(labelIndex < labelMap.size()) {
-                labelStr = labelMap[labelIndex];
-            }
-            cv::putText(frame, labelStr, cv::Point(x1 + 10, y1 + 20), cv::FONT_HERSHEY_TRIPLEX, 0.5, 255);
-            std::stringstream confStr;
-            confStr << std::fixed << std::setprecision(2) << detection.confidence * 100;
-            cv::putText(frame, confStr.str(), cv::Point(x1 + 10, y1 + 40), cv::FONT_HERSHEY_TRIPLEX, 0.5, 255);
-            cv::rectangle(frame, cv::Rect(cv::Point(x1, y1), cv::Point(x2, y2)), color, cv::FONT_HERSHEY_SIMPLEX);
-        }
-        // Show the frame
-        cv::imshow(name, frame);
-    };
+        // Properties
+        camRgb->setPreviewSize(nn_shape, nn_shape);
+        camRgb->setBoardSocket(dai::CameraBoardSocket::RGB);
+        camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+        camRgb->setInterleaved(false);
+        camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
+        camRgb->setFps(30);
 
-    while(true) {
-        // QueryPerformanceCounter(&time1);
-
-        // auto inRgb = qRgb->get<dai::ImgFrame>();
-
-        // QueryPerformanceCounter(&time2);
-
-        // double delay = double(time2.QuadPart - time1.QuadPart) / freq.QuadPart;
-
-        // cout << delay << "   " << 1/delay << endl;
-
-        // Retrieve 'bgr' (opencv format) frame
-        // cv::imshow("rgb", inRgb->getCvFrame());
-
-        // frm_cnt ++;
-
-        std::shared_ptr<dai::ImgFrame> inRgb;
-        std::shared_ptr<dai::ImgDetections> inDet;
-        // std::shared_ptr<dai::NNData> inDet;
-
+        // Linking
+        camRgb->preview.link(detectionNetwork->input);
         if(syncNN) {
-            inRgb = qRgb->get<dai::ImgFrame>();
-            inDet = qDet->get<dai::ImgDetections>();
-            // inDet = qDet->get<dai::NNData>();
+            detectionNetwork->passthrough.link(xoutRgb->input);
         } else {
-            inRgb = qRgb->tryGet<dai::ImgFrame>();
-            inDet = qDet->tryGet<dai::ImgDetections>();
-            // inDet = qDet->tryGet<dai::NNData>();
+            camRgb->preview.link(xoutRgb->input);
         }
 
-        counter++;
-        auto currentTime = steady_clock::now();
-        auto elapsed = duration_cast<duration<float>>(currentTime - startTime);
-        if(elapsed > seconds(1)) {
-            fps = counter / elapsed.count();
-            counter = 0;
-            startTime = currentTime;
+        detectionNetwork->out.link(nnOut->input);
+
+        // Connect to device and start pipeline
+        dai::Device device(pipeline, dai::UsbSpeed::SUPER);
+
+        cout << "Connected cameras: ";
+        for(const auto& cam : device.getConnectedCameras()) {
+            cout << cam << " ";
+        }
+        cout << endl;
+
+        // Print USB speed
+        cout << "Usb speed: " << device.getUsbSpeed() << endl;
+
+        // Output queue will be used to get the rgb frames from the output defined above
+        auto qRgb = device.getOutputQueue("rgb", 4, false);
+        auto qNN = device.getOutputQueue("nn", 4, false);
+
+        // LARGE_INTEGER time1, time2, freq, starttime, stoptime;
+        // QueryPerformanceFrequency(&freq);
+
+        // uint64_t frm_cnt = 0;
+        // QueryPerformanceCounter(&starttime);
+
+        cv::Mat frame;
+        // vector<dai::ImgDetection> detections;
+        vector<float> output;
+        auto startTime = steady_clock::now();
+        int counter = 0;
+        auto startTime_loop = startTime;
+        int counter_loop = 0;
+        float fps = 0;
+        auto color2 = cv::Scalar(255, 255, 255);
+        float duration_loop = 60;
+
+        // Add bounding boxes and text to the frame and show it to the user
+        auto displayFrame = [](std::string name, cv::Mat frame, vector<dai::ImgDetection>& detections) {
+        // auto displayFrame = [](std::string name, cv::Mat frame, vector<float>& detections) {
+            auto color = cv::Scalar(255, 0, 0);
+            // nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
+            for(auto& detection : detections) {
+                int x1 = detection.xmin * frame.cols;
+                int y1 = detection.ymin * frame.rows;
+                int x2 = detection.xmax * frame.cols;
+                int y2 = detection.ymax * frame.rows;
+
+                uint32_t labelIndex = detection.label;
+                std::string labelStr = to_string(labelIndex);
+                if(labelIndex < labelMap.size()) {
+                    labelStr = labelMap[labelIndex];
+                }
+                cv::putText(frame, labelStr, cv::Point(x1 + 10, y1 + 20), cv::FONT_HERSHEY_TRIPLEX, 0.5, 255);
+                std::stringstream confStr;
+                confStr << std::fixed << std::setprecision(2) << detection.confidence * 100;
+                cv::putText(frame, confStr.str(), cv::Point(x1 + 10, y1 + 40), cv::FONT_HERSHEY_TRIPLEX, 0.5, 255);
+                cv::rectangle(frame, cv::Rect(cv::Point(x1, y1), cv::Point(x2, y2)), color, cv::FONT_HERSHEY_SIMPLEX);
+            }
+            // Show the frame
+            cv::imshow(name, frame);
+        };
+
+        startTime_loop = steady_clock::now();
+
+        // while(true) {
+        while(duration_cast<duration<float>>(steady_clock::now() - startTime_loop).count() < duration_loop ) {
+            std::shared_ptr<dai::ImgFrame> inRgb;
+            std::shared_ptr<dai::NNData> inNN;
+
+            if(syncNN) {
+                inRgb = qRgb->get<dai::ImgFrame>();
+                inNN = qNN->get<dai::NNData>();
+            } else {
+                inRgb = qRgb->tryGet<dai::ImgFrame>();
+                inNN = qNN->tryGet<dai::NNData>();
+            }
+
+            counter++;
+            counter_loop++;
+            auto currentTime = steady_clock::now();
+            auto elapsed = duration_cast<duration<float>>(currentTime - startTime);
+            if(elapsed > seconds(1)) {
+                fps = counter / elapsed.count();
+                counter = 0;
+                startTime = currentTime;
+            }
+
+            if(inRgb) {
+                frame = inRgb->getCvFrame();
+                std::stringstream fpsStr;
+                fpsStr << "NN fps: " << std::fixed << std::setprecision(2) << fps;
+                cv::putText(frame, fpsStr.str(), cv::Point(2, inRgb->getHeight() - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color2);
+            }
+
+            if(inNN) {
+                output = inNN->getLayerFp16("output");
+                // cout << output.size() << endl;
+            }
+
+            if(!frame.empty()) {
+                // displayFrame("rgb", frame, detections);
+                cv::imshow("rgb", frame);
+            }
+
+            int key = cv::waitKey(1);
+            if(key == 'q' || key == 'Q') {
+                break;
+                // return 0;
+            }
         }
 
-        if(inRgb) {
-            frame = inRgb->getCvFrame();
-            std::stringstream fpsStr;
-            fpsStr << "NN fps: " << std::fixed << std::setprecision(2) << fps;
-            cv::putText(frame, fpsStr.str(), cv::Point(2, inRgb->getHeight() - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color2);
-        }
+        // QueryPerformanceCounter(&stoptime);
+        // cout << frm_cnt << "   " << frm_cnt / (double(stoptime.QuadPart - starttime.QuadPart) / freq.QuadPart) << endl;
+        
+        cout << counter_loop << "   " << counter_loop / duration_cast<duration<double>>(steady_clock::now() - startTime_loop).count() << endl;
+        fs << counter_loop << "   " << counter_loop / duration_cast<duration<double>>(steady_clock::now() - startTime_loop).count() << endl;
 
-        if(inDet) {
-            detections = inDet->detections;
-            // detections = inDet->getLayerFp16("output");
-        }
-
-        if(!frame.empty()) {
-            displayFrame("rgb", frame, detections);
-        }
-
-        int key = cv::waitKey(1);
-        if(key == 'q' || key == 'Q') {
-            return 0;
-        }
     }
 
-    // QueryPerformanceCounter(&stoptime);
-    // cout << frm_cnt << "   " << frm_cnt / (double(stoptime.QuadPart - starttime.QuadPart) / freq.QuadPart) << endl;
+    fs.close();
 
     return 0;
 }
