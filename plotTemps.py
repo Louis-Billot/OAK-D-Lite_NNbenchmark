4e4797fcaf1a44c2ba6c7234ae4da44c32ca856b
Louis-Billot <96132026+Louis-Billot@users.noreply.github.com>
2022-05-02 15:47:50 +0200

    temps logs in directory

--- plotTemps.py
+++ plotTemps.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # f = open('2022-04-28_11-05-18.log', 'r')
 # f = open('2022-04-29_10-28-59.log', 'r')
 # f = open('2022-04-29_10-38-07.log', 'r')
-f = open('2022-04-29_11-05-05.log', 'r')
+f = open('./temps_logs/2022-04-29_11-05-05.log', 'r')
 
 temps = [float(line[:-1]) for line in f.readlines()]
 
